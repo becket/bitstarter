703106e9ed16d888ccecd6769f140cb961730cb7
John Smith <example@stanford.edu>
2013-07-10 12:16:06 +0000

    Se vuelve a poner app.get. Corregido  web.js para que lea archivo index.html

--- web.js
+++ web.js
@@ -5,7 +5,11 @@ var app = express.createServer(express.logger());
 
 var buffer = fs.readFileSync('index.html');
 
-buffer.toString('utf-8');
+var cadena = buffer.toString('utf-8');
+
+app.get('/', function(request, response) {
+    response.send(cadena);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
